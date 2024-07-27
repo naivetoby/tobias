e084fdcad6dad26b51186c199892a42d99c9aacf
陶开杰 <naivetoby@gmail.com>
2024-07-27 16:04:50 +0800

    移除 iOS 相关 SDK
    移除 iOS 相关 SDK

--- lib/src/tobias.dart
+++ lib/src/tobias.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'config.dart';
 import 'tobias_platform_interface.dart';
 
@@ -6,27 +8,32 @@ class Tobias {
   /// [universalLink] only supports iOS
   Future<Map> pay(String order,
       {AliPayEvn evn = AliPayEvn.online, String? universalLink}) async {
+    if (Platform.isIOS) throw UnimplementedError('pay() has not been implemented.');
     return await TobiasPlatform.instance
         .pay(order, evn: evn, universalLink: universalLink);
   }
 
   /// Auth by AliPay
   Future<Map> auth(String auth) async {
+    if (Platform.isIOS) throw UnimplementedError('pay() has not been implemented.');
     return await TobiasPlatform.instance.auth(auth);
   }
 
   /// AliPay version
   Future<String> get aliPayVersion async {
+    if (Platform.isIOS) throw UnimplementedError('pay() has not been implemented.');
     return await TobiasPlatform.instance.aliPayVersion;
   }
 
   /// If the user has installed AliPay
   Future<bool> get isAliPayInstalled async {
+    if (Platform.isIOS) throw UnimplementedError('pay() has not been implemented.');
     return await TobiasPlatform.instance.isAliPayInstalled;
   }
 
   /// If the user has installed AliPayHk
   Future<bool> get isAliPayHKInstalled async {
+    if (Platform.isIOS) throw UnimplementedError('pay() has not been implemented.');
     return await TobiasPlatform.instance.isAliPayHKInstalled;
   }
 }
